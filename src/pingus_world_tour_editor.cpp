5d948772e739d12a04d581983c6a306ec01edf5f
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-04-10 19:38:32 +0200

    working on pwt level editor

--- src/pingus_world_tour_editor.cpp
+++ src/pingus_world_tour_editor.cpp
@@ -21,6 +21,9 @@ void PingusWorldTourEditor::OnLevelEnter(Level* _level){
     b_import_layer = AddChild<Button>(Vector2f(32.0f*2,0.0f),Vector2f(56.0f,0.0f),"Import Layer");
     b_import_layer->adjust_height_after_text_rows = true;
     m_layers = AddChild<WrapMenu>(Vector2f(GetRectangle().size.x-200.0f,0.0f),Vector2f(200.0f,200.0f));
+    AddChild<Button>(GetRectangle().size-Vector2f(32.0f,32.0f), Vector2f(32.0f,32.0f), "S");
+    AddChild<Button>(GetRectangle().size-Vector2f(64.0f,32.0f), Vector2f(32.0f,32.0f), "P");
+    AddChild<Button>(GetRectangle().size-Vector2f(96.0f,32.0f), Vector2f(32.0f,32.0f), "C");
     AddLayer("solid", "solid.png");
     AddLayer("mg", "mg.png");
     
