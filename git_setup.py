a3eb3c84fe650d59e2939f75dd11346aeaba8c49
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2025-05-16 17:22:54 +0200

    git_setup.py

--- git_setup.py
+++ git_setup.py
@@ -4,3 +4,5 @@ ret : int = os.system("git submodule update --init --recursive")
 
 if ret:
     print("Git repository fully set up")
+    if not os.system("python start.py"):
+        os.system("python3 start.py")
